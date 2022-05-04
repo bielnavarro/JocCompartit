3d1f38cf3cf239609424635229e2909340ecf782
Gerardmasblanch <101257175+Gerardmasblanch@users.noreply.github.com>
2022-05-04 08:55:14 +0200

    cotxesmovense
    ddd

---
+++ cotxerapid.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+var velocitat_base = 5000
+var velocitat = Vector2.ZERO
+var gravetat = Vector2.LEFT * 14000
+
+func _physics_process(delta):
+	
+	velocitat.x = 0
+	velocitat.y = 0
+	
+	velocitat += gravetat * delta
+	velocitat = move_and_slide(velocitat)
