a48673734d6d032ba6bb7381057c76b51440b0ce
Gerardmasblanch <101257175+Gerardmasblanch@users.noreply.github.com>
2022-05-13 10:54:40 +0200

    acabar menu
    ...

--- cotxelladre.gd
+++ cotxelladre.gd
@@ -4,18 +4,17 @@ var velocitat_base = 200
 var velocitat = Vector2.ZERO
 var velocitat_amunt = Vector2.UP * 400
 var velocitta_avall = Vector2.DOWN * 400
+var gravetat = Vector2.RIGHT * 10000
 
 func _physics_process(delta):
-	
-	velocitat.y = 0
-	velocitat.x = 0
-	
-	if Input.is_action_pressed("mou dreta"):
-		velocitat += Vector2.RIGHT * velocitat_base
 	if Input.is_action_just_pressed("mou amunt"):
 		velocitat += velocitat_amunt
 	if Input.is_action_just_pressed("mou avall"):
 		velocitat += velocitta_avall
 	
+	velocitat.x = 0
+	velocitat.y = 0
+	
+	velocitat += gravetat * delta
+	velocitat = move_and_slide(velocitat)
 	velocitat = move_and_slide(velocitat, Vector2.UP)
-
