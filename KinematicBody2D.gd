f4c681ef4c8819cb8e9973fc4e7288aa00bfe83c
JonPG <jon.papell@gmail.com>
2022-05-16 12:30:52 +0200

    Personatge i priova escena

---
+++ KinematicBody2D.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+var velocitat_base = 200
+var gravetat = Vector2.DOWN * 980
+var velocitat =  Vector2.ZERO
+var salt = Vector2.UP * 350
+
+func _physics_process(delta):
+	
+	velocitat.x = 0
+	
+	if Input.is_action_pressed("mou dreta"):
+		velocitat += Vector2.RIGHT * velocitat_base
+	if Input.is_action_pressed("mou esquerra"):
+		velocitat += Vector2.LEFT * velocitat_base
+	if Input.is_action_just_pressed("mou adalt") and is_on_floor():
+		velocitat += salt
+	
+	
+	
+	velocitat += gravetat * delta
+	print(velocitat)
+	velocitat = move_and_slide(velocitat, Vector2.UP)
+	
+	anima(velocitat)
+
+func anima(velocitat: Vector2):
+	var animacio : AnimatedSprite = $AnimatedSprite
+	if velocitat.x > 0:
+		animacio.flip_h = false
+		animacio.play('run')
+	elif velocitat.x < 0:
+		animacio.flip_h = true
+		animacio.play('run')
+	
+	if velocitat.y < -1:
+		animacio.play('jump')
