43e439bc25dbe01d5155be0400eadef3c8acdc06
Gerardmasblanch <101257175+Gerardmasblanch@users.noreply.github.com>
2022-05-16 12:29:04 +0200

    jkui
    kyukyu

--- cotxelladre.gd
+++ cotxelladre.gd
@@ -1,20 +1,59 @@
 extends KinematicBody2D
 
-var velocitat_base = 200
+var inici = Vector2(50,275)
+var posicio = inici
+var velocitat_base = 400
 var velocitat = Vector2.ZERO
-var velocitat_amunt = Vector2.UP * 400
-var velocitta_avall = Vector2.DOWN * 400
-var gravetat = Vector2.RIGHT * 10000
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("mou amunt"):
-		velocitat += velocitat_amunt
-	if Input.is_action_just_pressed("mou avall"):
-		velocitat += velocitta_avall
+	velocitat = Vector2.ZERO
+	velocitat.x += 300
+	if Input.is_action_pressed("mou amunt"):
+		velocitat.y = -velocitat_base
+	if Input.is_action_pressed("mou avall"):
+		velocitat.y = velocitat_base
+		
+	velocitat.normalized()
+	move_and_slide(velocitat)
 	
-	velocitat.x = 0
-	velocitat.y = 0
-	
-	velocitat += gravetat * delta
-	velocitat = move_and_slide(velocitat)
-	velocitat = move_and_slide(velocitat, Vector2.UP)
+
+
+func _on_policia1_body_entered(body):
+	get_tree().reload_current_scene()
+
+
+func _on_policia2_body_entered(body):
+	get_tree().reload_current_scene()
+
+
+func _on_policia3_body_entered(body):
+	get_tree().reload_current_scene()
+
+
+func _on_policia4_body_entered(body):
+	get_tree().reload_current_scene()
+
+
+func _on_camio_body_entered(body):
+	get_tree().reload_current_scene()
+
+
+func _on_camio2_body_entered(body):
+	get_tree().reload_current_scene()
+
+func _on_cotxerapid_body_entered(body):
+	get_tree().reload_current_scene()
+
+
+func _on_bombero_body_entered(body):
+	get_tree().reload_current_scene()
+
+func _on_bombero2_body_entered(body):
+	get_tree().reload_current_scene()
+
+func _on_cotxeraro_body_entered(body):
+	get_tree().reload_current_scene()
+
+
+func _on_cotxeraro2_body_entered(body):
+	get_tree().reload_current_scene()
--- cotxerapid.gd
+++ cotxerapid.gd
@@ -1,13 +1,4 @@
-extends KinematicBody2D
-
-var velocitat_base = 5000
-var velocitat = Vector2.ZERO
-var gravetat = Vector2.LEFT * 14000
-
+extends Area2D
+var posicio = Vector2(763,-182)
 func _physics_process(delta):
-	
-	velocitat.x = 0
-	velocitat.y = 0
-	
-	velocitat += gravetat * delta
-	velocitat = move_and_slide(velocitat)
+	position.x += -6
